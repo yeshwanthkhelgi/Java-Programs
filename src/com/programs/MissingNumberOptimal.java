9812600372fe8ee14678bcf34e4a04e0cd29f065
yeshwanthkhelgi <yeshwanthkhelgi@gmail.com>
2025-04-21 19:38:02 +0530

    missing values

--- src/com/programs/MissingNumberOptimal.java
+++ src/com/programs/MissingNumberOptimal.java
@@ -1,23 +1,28 @@
 package com.programs;
 
-public class MissingNumber {
+public class MissingNumberOptimal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] arr = {1,2,3,4};
+		int[] arr = {1,2,3,5};
 		System.out.println(missingNumber(arr,5));
 
 	}
 
 	private static int missingNumber(int[] arr,int k) {
 		// TODO Auto-generated method stub
+		int xor1 = 0;
+		int xor2 = 0;
 		int n = arr.length;
-		int sum = k*(k+1)/2;
-		int s = 0;
-		for(int a:arr) {
-			s = s + a;
+//		for(int i=1;i<=k;i++) {
+//			xor1 = xor1^i;
+//		}
+		for(int i=0;i<n;i++) {
+			xor2 = xor2^arr[i];
+			xor1 = xor1^(i+1);
 		}
-		return sum - s;
+		xor1 = xor1^k;
+		return xor1^xor2;
 	}
 
 }
